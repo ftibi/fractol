f1fc50d7349d2177e87bd40f3aab7388e5f7ba9d
tfolly <tfolly@student.42.fr>
2016-05-18 20:41:41 +0200

    pblm de param julia avec le reset etc

--- sources/key_fct.c
+++ sources/key_fct.c
@@ -6,7 +6,7 @@
 /*   By: tfolly <tfolly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/17 18:36:10 by tfolly            #+#    #+#             */
-/*   Updated: 2016/05/18 18:48:01 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/05/18 20:37:43 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,19 @@ int		my_key_funct(int keycode, t_frac *frac)
 	}
 	else if (keycode == 15)
 	{
-		mlx_clear_window(frac->mlx, frac->win);
 		frac = frac_init(frac->ens, frac->mlx, frac->win, frac->img);
+		mlx_clear_window(frac->mlx, frac->win);
 		frac->aff_frac(frac);
 	}
 	else if (keycode == 38)
 		frac->juliamove = (frac->juliamove) ? 0 : 1;
 	else if (keycode == 40)
 	{
+		frac = frac_init(frac->ens, frac->mlx, frac->win, frac->img);
 		frac->juliamove = 0;
 		frac->juliax = 0.285;
 		frac->juliay = 0.01;
+		mlx_clear_window(frac->mlx, frac->win);
 		frac->aff_frac(frac);
 	}
 	return (0);
