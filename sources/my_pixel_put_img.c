60584884552972017161e0cbc31e3fbf9a4f70b4
tfolly <tfolly@student.42.fr>
2016-05-18 20:15:10 +0200

    x4 RESOLUE XD

--- sources/my_pixel_put_img.c
+++ sources/my_pixel_put_img.c
@@ -6,7 +6,7 @@
 /*   By: tfolly <tfolly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/18 16:58:19 by tfolly            #+#    #+#             */
-/*   Updated: 2016/05/18 20:03:46 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/05/18 20:14:40 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ void		my_pixel_put_img(t_frac *frac, int x, int y, int color)
 	color3 = (ucolor & 0xFF);
 	// frac->data[x * frac->bpp / 8 + (int)frac->size * y] = mlx_get_color_value(frac->mlx, color);
 	// frac->data[(int)frac->size * y + x * frac->bpp / 8 ] = (unsigned char)color;
-	frac->data[x + (int)frac->size * y] = (unsigned char)color1;
-	frac->data[x + (int)frac->size * y + 1] = (unsigned char)color2;
-	frac->data[x + (int)frac->size * y + 2] = (unsigned char)color3;
+	frac->data[frac->bpp / 8 * (x + (int)frac->size * y)] = (unsigned char)color1;
+	frac->data[frac->bpp / 8 * (x + (int)frac->size * y) + 1] = (unsigned char)color2;
+	frac->data[frac->bpp / 8 * (x + (int)frac->size * y) + 2] = (unsigned char)color3;
 }
