c46c8c7c1108d1656e3eb1f9f9d3e458c4a71ad5
tfolly <tfolly@student.42.fr>
2016-05-19 15:11:41 +0200

    manuel

--- sources/manuel.c
+++ sources/manuel.c
@@ -6,7 +6,7 @@
 /*   By: tfolly <tfolly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/25 18:34:24 by tfolly            #+#    #+#             */
-/*   Updated: 2016/04/25 18:38:57 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/05/19 15:11:25 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 void	manuel(void)
 {
 	ft_putendl("use mousewheel to zoom in and out");
+	ft_putendl("use arrow keys to move around");
 	ft_putendl("J   : toggle Julia's parameter change");
 	ft_putendl("K   : reset Julia's parameters");
+	ft_putendl("+ - : change interrations");
+	ft_putendl("C   : change color panel");
 	ft_putendl("ESC : exit");
 }
